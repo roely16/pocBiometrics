ca308e814fc55ebf9f878245c03819b2b6a7fbc9
Herson Chur <hchur@eaglehubs.com>
2023-08-18 10:01:30 -0600

    last tests

--- App.js
+++ App.js
@@ -28,14 +28,6 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import ReactNativeBiometrics, {BiometryTypes} from 'react-native-biometrics';
-import {
-  generateKeyPair,
-  encrypt,
-  sign,
-  decrypt,
-  getKey,
-} from 'react-native-secure-encryption-module';
-// import ECEncryption from 'react-native-ec-encryption';
 import DeviceCrypto, {AccessLevel, KeyTypes} from 'react-native-device-crypto';
 const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -64,156 +56,30 @@ const Section = ({children, title}): Node => {
 };
 
 const App: () => Node = () => {
-  const backgroundStyle = {
-    backgroundColor: Colors.lighter,
-  };
-
-  const handleBiometris = async () => {
-    try {
-      const rnBiometrics = new ReactNativeBiometrics();
-
-      const {biometryType} = await rnBiometrics.isSensorAvailable();
-
-      console.log('biometry type => ', biometryType);
-
-      let epochTimeSeconds = Math.round(new Date().getTime() / 1000).toString();
-      let payload = epochTimeSeconds + 'some message';
-
-      rnBiometrics
-        .createSignature({
-          promptMessage: 'Sign in',
-          payload: payload,
-        })
-        .then(resultObject => {
-          const {success, signature} = resultObject;
-
-          if (success) {
-            console.log(signature);
-            verifySignatureWithServer(signature, payload);
-          }
-        });
-    } catch (error) {
-      console.log('error => ', error);
-    }
-  };
-
-  const createKeys = () => {
-    const rnBiometrics = new ReactNativeBiometrics();
-
-    rnBiometrics.createKeys().then(resultObject => {
-      const {publicKey} = resultObject;
-      console.log('createKeys => ', publicKey);
-    });
-  };
-
-  const createSignature = () => {
-    let epochTimeSeconds = Math.round(new Date().getTime() / 1000).toString();
-    let payload = 'a';
-
-    const rnBiometrics = new ReactNativeBiometrics();
-
-    rnBiometrics
-      .createSignature({
-        promptMessage: 'Sign in',
-        payload: payload,
-      })
-      .then(resultObject => {
-        const {success, signature} = resultObject;
-
-        if (success) {
-          console.log('createSignature => ', signature);
-        }
-      });
-  };
-
-  const isSensorAvailable = () => {
-    const rnBiometrics = new ReactNativeBiometrics();
-
-    rnBiometrics.isSensorAvailable().then(resultObject => {
-      const {available, biometryType} = resultObject;
-
-      if (available && biometryType === BiometryTypes.TouchID) {
-        console.log('TouchID is supported');
-      } else if (available && biometryType === BiometryTypes.FaceID) {
-        console.log('FaceID is supported');
-      } else if (available && biometryType === BiometryTypes.Biometrics) {
-        console.log('Biometrics is supported');
-      } else {
-        console.log('Biometrics not supported');
-      }
-    });
-  };
-
-  // const deleteKeys = () => {
-  //   const rnBiometrics = new ReactNativeBiometrics();
-
-  //   rnBiometrics.deleteKeys().then(resultObject => {
-  //     const {keysDeleted} = resultObject;;
-
-  //     if (keysDeleted) {
-  //       console.log('Successful deletion');;
-  //     } else {
-  //       console.log(
-  //         'Unsuccessful deletion because there were no keys to delete',;
-  //       );
-  //     }
-  //   });
-  // };
-
-  const getKeyPair = async () => {
-    const key = await generateKeyPair('my-key');
-    console.log('generate key pair => ', key);
-    const returnKey = await getKey('my-key');
-    console.log('return key => ', returnKey);
-    const cipher = await encrypt('Encrypt this message', 'my-key');
-    console.log('cipher message => ', cipher);
-    const sign_message = await sign('Sign this message', 'my-key');
-    console.log('sign message => ', sign_message);
-    const clearText = await decrypt(cipher, 'my-key');
-    console.log('message decrypt => ', clearText);
-  };
-
-  // const testReactNativeEC = async () => {
-  //   try {
-  //     //Encrypt
-  //     const cipherText = await ECEncryption.encrypt({
-  //       data: 'some confidential data',
-  //       label: '0x5Cc5dc62be3c95C771C14232e30358B398265deF', //any identical string
-  //     });
-  //     //Decrypt
-  //     const clearText = await ECEncryption.decrypt({
-  //       data: cipherText,
-  //       label: '0x5Cc5dc62be3c95C771C14232e30358B398265deF', //the same identical string
-  //     });
-  //     console.log('decrypt result is ', clearText);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // };
 
   // Test react-native-device-crypto
 
-  const [accessLevel, setAccessLevel] = React.useState<AccessLevel>(2);
+  const [accessLevel, setAccessLevel] = React.useState<AccessLevel>(AccessLevel.AUTHENTICATION_REQUIRED);
 
   const testReactNativeDeviceCrypto = async () => {
     // create asymetric key with FaceID
-    const key = await DeviceCrypto.getOrCreateAsymmetricKey('my-key', {
+    const key = await DeviceCrypto.getOrCreateSymmetricKey('my-key', {
       accessLevel,
       invalidateOnNewBiometry: true,
     });
     console.log('asymetric key => ', key);
 
-    // get public key
-    const publicKey = await DeviceCrypto.getPublicKey('my-key');
-    console.log('public key => ', publicKey);
+    // // get public key
+    // const publicKey = await DeviceCrypto.getPublicKey('my-key');
+    // console.log('public key => ', publicKey);
 
-    // Sign
-    const sign = await DeviceCrypto.sign('my-key', 'text to sign', {
-      biometryTitle: 'Authenticate',
-      biometrySubTitle: 'Signing',
-      biometryDescription: 'Authenticate your self to sign the text',
-    });
-    console.log('text signed => ', sign);
+    // // Sign
+    // const sign = await DeviceCrypto.sign('my-key', 'text to sign', {
+    //   biometryTitle: 'Authenticate',
+    //   biometrySubTitle: 'Signing',
+    //   biometryDescription: 'Authenticate your self to sign the text',
+    // });
+    // console.log('text signed => ', sign);
 
     //Encrypt
     const encrypt = await DeviceCrypto.encrypt('my-key', 'text to encrypt', {
@@ -230,7 +96,7 @@ const App: () => Node = () => {
       encrypt.iv,
       {
         biometryTitle: 'Authenticate',
-        biometrySubTitle: 'Encrypt',
+        biometrySubTitle: 'Decrypt',
         biometryDescription: 'Authenticate your self to encrypt the text',
       },
     );
@@ -241,23 +107,48 @@ const App: () => Node = () => {
     await DeviceCrypto.deleteKey('my-key');
   };
 
+  const isBiometryEnrolled = async () => {
+    const isEnrolled = await DeviceCrypto.isBiometryEnrolled()
+
+    if (isEnrolled) {
+      const biometryType = await DeviceCrypto.getBiometryType();
+      console.log('biometry type => ', biometryType);
+
+      const deviceSecurityLevel = await DeviceCrypto.deviceSecurityLevel();
+      console.log('device security leve => ', deviceSecurityLevel);
+    }
+    console.log('is biometry enrolled => ', isEnrolled)
+  }
+
+  // react-native-biometrics
+  const rnBiometrics = new ReactNativeBiometrics({ allowDeviceCredentials: true })
+
+  const createKeys = () => {
+    rnBiometrics.createKeys()
+    .then((resultObject) => {
+      const { publicKey } = resultObject
+      console.log(publicKey)
+    })
+  };
+
+  const createSignature = () => {
+    let epochTimeSeconds = Math.round((new Date()).getTime() / 1000).toString()
+    let payload = epochTimeSeconds + 'some message'
+
+    rnBiometrics.createSignature({
+      promptMessage: 'Sign in',
+      payload: payload
+    })
+    .then((resultObject) => {
+      const { success, signature } = resultObject
+  
+      if (success) {
+        console.log(signature)
+      }
+    })
+  };
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      {/* <TouchableOpacity style={{marginBottom: 20}} onPress={createKeys}>
-        <Text style={{color: 'black'}}>Create Keys</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={{marginBottom: 20}} onPress={createSignature}>
-        <Text style={{color: 'black'}}>Create Signature</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={{marginBottom: 20}} onPress={isSensorAvailable}>
-        <Text style={{color: 'black'}}>isSensorAvailable</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={{marginBottom: 20}} onPress={getKeyPair}>
-        <Text style={{color: 'black'}}>Generate a KeyPair</Text>
-      </TouchableOpacity> */}
-      {/* <TouchableOpacity style={{marginBottom: 20}} onPress={testReactNativeEC}>
-        <Text style={{color: 'black'}}>Test React-Native-EC-Encryption</Text>
-      </TouchableOpacity> */}
       <TouchableOpacity
         style={{marginBottom: 20}}
         onPress={testReactNativeDeviceCrypto}>
@@ -265,14 +156,26 @@ const App: () => Node = () => {
       </TouchableOpacity>
       <TouchableOpacity
         style={{marginBottom: 20}}
+        onPress={isBiometryEnrolled}>
+        <Text style={{color: 'black'}}>Check if biometry is enrolled</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{marginBottom: 20}}
         onPress={testReactNativeDeviceCryptoDelete}>
-        <Text style={{color: 'black'}}>
+        <Text style={{color: 'black', marginBottom: 50}}>
           Delete Key react-native-device-crypto
         </Text>
       </TouchableOpacity>
-      {/* <TouchableOpacity onPress={deleteKeys}>
-        <Text style={{color: 'black'}}>Delete Keys</Text>
-      </TouchableOpacity> */}
+      <TouchableOpacity
+        style={{marginBottom: 20}}
+        onPress={createKeys}>
+        <Text style={{color: 'black'}}>createKeys</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{marginBottom: 20}}
+        onPress={createSignature}>
+        <Text style={{color: 'black'}}>createSignature</Text>
+      </TouchableOpacity>
     </View>
   );
 };
